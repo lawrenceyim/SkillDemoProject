4a92883c9bcb79b88b721428ab558937bf85880e
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-05-24 18:07:58 -0400

    Added popup manager

--- Scripts/Item/InteractableItem.cs
+++ Scripts/Item/InteractableItem.cs
@@ -1,21 +1,39 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
 public partial class InteractableItem : BaseItem, IInteractable
 {
-	[Signal] public delegate void PopupBoxEventHandler(bool display, string message);
-
+	[Signal] public delegate void PopupBoxEventHandler(string message);
+	[Export] protected PopupManager popupManager;
 	[Export] protected Area2D interactionArea;
 	[Export] protected Sprite2D interactionSprite;
 	[Export] protected PlayerInputHandler playerInputHandler;
 	private bool canInteractWith = false;
+	private DialogueTree dialogueTree;
 
 	public override void _Ready() {
 		interactionArea.BodyEntered += AddInteractionIndicator;
 		interactionArea.BodyExited += RemoveInteractionIndicator;
 		playerInputHandler.Interaction += InteractWith;
 
+		dialogueTree = new DialogueTree(new DialogueNode("Your mailbox is empty."));
+		dialogueTree.OnQuestionSent += QuestionReceived;
+		dialogueTree.OnDialogueSent += DialogueReceived;
+
 		interactionSprite.Visible = false;
+	}
+
+	private void DialogueReceived(object sender, List<string> e) {
+		foreach(string message in e) {
+			GD.Print(message);
+			popupManager.ShowPopupPanel();
+			popupManager.ChangePopupLabelText(message);
+		}
+	}
+
+	private void QuestionReceived(object sender, (string, List<string>) e) {
+		throw new NotImplementedException();
 	}
 
 	private void RemoveInteractionIndicator(Node2D body)
@@ -24,6 +42,7 @@ public partial class InteractableItem : BaseItem, IInteractable
 			interactionSprite.Visible = false;
 			canInteractWith = false;
 			EmitSignal(SignalName.PopupBox, true, "");
+			popupManager.HidePopupPanel();
 		}
 	}
 
@@ -40,6 +59,7 @@ public partial class InteractableItem : BaseItem, IInteractable
 	{
 		if (canInteractWith) {
 			GD.Print("Interacted with mail box");
+			dialogueTree.Evaluate();
 		}
 	}
 
